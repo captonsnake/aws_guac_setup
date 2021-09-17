9eb7d0b90bad7a3063e1baf022fe3b78af24b815
Ubuntu <ubuntu@ip-172-31-31-120.us-east-2.compute.internal>
2021-09-17 15:39:54 +0000

    fix install script maybe

--- install.sh
+++ install.sh
@@ -1,46 +1,48 @@
-#!/bin/sh
+#!/bin/bash
 #
 echo "Installing Dependency"
-apt-get -qq update
-apt-get -qq install -y tightvncserver xfce4 xfce4-goodies docker.io docker-compose
+sudo apt-get -qq update
+sudo apt-get -qq install -y tightvncserver xfce4 xfce4-goodies docker.io docker-compose
 echo "Configuring tightVNC"
-echo "Enter a Password for VNC"
+echo "Enter a Password for VNC if not already ran"
 vncserver
 if [ $? -eq 0]; then
 	echo "VNC password set";
 else
 	echo "VNC password failed to set. Must be 8 characters"
+	vncserver -kill :1
 	exit 1
+fi
 vncserver -kill :1
 echo "Setting vnc desktop as xfce4"
 echo 'startxfce4 &' >> $HOME/.vnc/xstartup
 echo "Moving dirs"
-mkdir /opt/guac_services/
-cp docker-compose.yml /opt/guac_services/
-cp -r nginx/ /opt/guac_services/.
+sudo mkdir /opt/guac_services/
+sudo cp docker-compose.yml /opt/guac_services/
+sudo cp -r nginx/ /opt/guac_services/.
 echo "Preparing folder init and creating /opt/guac_services/init/initdb.sql"
-mkdir /opt/guac_services/init >/dev/null 2>&1
-mkdir -p /opt/guac_services/nginx/ssl >/dev/null 2>&1
-chmod -R +x /opt/guac_services/init
+sudo mkdir /opt/guac_services/init >/dev/null 2>&1
+sudo mkdir -p /opt/guac_services/nginx/ssl >/dev/null 2>&1
+sudo chmod -R +x /opt/guac_services/init
 docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgres > /opt/guac_services/init/initdb.sql
 echo "Creating SSL certificates"
-openssl req -nodes -newkey rsa:2048 -new -x509 -keyout /opt/guac_services/nginx/ssl/self-ssl.key -out /opt/guac_services/nginx/ssl/self.cert -subj '/C=US/ST=MD/L=Baltimore/O=Company/OU=IT/CN=www.custom.domain'
+sudo openssl req -nodes -newkey rsa:2048 -new -x509 -keyout /opt/guac_services/nginx/ssl/self-ssl.key -out /opt/guac_services/nginx/ssl/self.cert -subj '/C=US/ST=MD/L=Baltimore/O=Company/OU=IT/CN=www.custom.domain'
 echo "Setting Up Guacamole Service"
-cp guacamole.service /etc/systemd/system/.
+sudo cp guacamole.service /etc/systemd/system/.
 chown root:root /etc/systemd/system/guacamole.service
-chmod 0644 /etc/systemd/system/guacamole.service
+sudo chmod 0644 /etc/systemd/system/guacamole.service
 echo "Setting Up VNC Service"
 sed -i "s|USER_HOME|$HOME|g" vncserver.service
-cp vncserver.service /etc/systemd/system/.
-chown root:root /etc/systemd/system/vncserver.service
-chmod 0644 /etc/systemd/system/vncserver.service
-systemctl daemon-reload
+sudo cp vncserver.service /etc/systemd/system/.
+sudo chown root:root /etc/systemd/system/vncserver.service
+sudo chmod 0644 /etc/systemd/system/vncserver.service
+sudo systemctl daemon-reload
 echo "enabling Services"
-systemctl enable guacamole.service
-systemctl enable vncserver.service
+sudo systemctl enable guacamole.service
+sudo systemctl enable vncserver.service
 echo "Starting Services"
-systemctl start vncserver.service
-systemctl start guacamole.service
+sudo systemctl start vncserver.service
+sudo systemctl start guacamole.service
 echo "IMPORTANT: Navigate to your guacamole at https://<url or IP>/guac and change the Default User"
 echo "Default user credentials: guacadmin/guacadmin"
 echo "You can use your own certificates by placing the private key in /opt/guac_services/nginx/ssl/self-ssl.key and the cert in /opt/guac_services/nginx/ssl/self.cert"
