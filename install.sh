d94f5882046d4479c30df1a6680951c4e4a4a834
Ubuntu <ubuntu@ip-172-31-31-120.us-east-2.compute.internal>
2021-09-17 14:48:13 +0000

    fix vncservice

--- install.sh
+++ install.sh
@@ -2,7 +2,18 @@
 #
 echo "Installing Dependency"
 apt-get -qq update
-apt-get -qq install -y tightvncserver xfce4 docker.io docker-compose
+apt-get -qq install -y tightvncserver xfce4 xfce4-goodies docker.io docker-compose
+echo "Configuring tightVNC"
+echo "Enter a Password for VNC"
+vncserver
+if [ $? -eq 0]; then
+	echo "VNC password set";
+else
+	echo "VNC password failed to set. Must be 8 characters"
+	exit 1
+vncserver -kill :1
+echo "Setting vnc desktop as xfce4"
+echo 'startxfce4 &' >> $HOME/.vnc/xstartup
 echo "Moving dirs"
 mkdir /opt/guac_services/
 cp docker-compose.yml /opt/guac_services/
@@ -14,12 +25,14 @@ chmod -R +x /opt/guac_services/init
 docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgres > /opt/guac_services/init/initdb.sql
 echo "Creating SSL certificates"
 openssl req -nodes -newkey rsa:2048 -new -x509 -keyout /opt/guac_services/nginx/ssl/self-ssl.key -out /opt/guac_services/nginx/ssl/self.cert -subj '/C=US/ST=MD/L=Baltimore/O=Company/OU=IT/CN=www.custom.domain'
-echo "Setting Up Services"
-cp .guacamole.service /etc/systemd/system/.
-chown /etc/systemd/system/guacamole.service root:root
+echo "Setting Up Guacamole Service"
+cp guacamole.service /etc/systemd/system/.
+chown root:root /etc/systemd/system/guacamole.service
 chmod 0644 /etc/systemd/system/guacamole.service
-cp .vncserver.service /etc/systemd/system/.
-chown /etc/systemd/system/vncserver.service root:root
+echo "Setting Up VNC Service"
+sed -i "s|USER_HOME|$HOME|g" vncserver.service
+cp vncserver.service /etc/systemd/system/.
+chown root:root /etc/systemd/system/vncserver.service
 chmod 0644 /etc/systemd/system/vncserver.service
 systemctl daemon-reload
 echo "enabling Services"
@@ -28,7 +41,7 @@ systemctl enable vncserver.service
 echo "Starting Services"
 systemctl start vncserver.service
 systemctl start guacamole.service
-echo "IMPORTANT: Navigate to your guacamole at <my_url or IP/guac and change the Default User"
+echo "IMPORTANT: Navigate to your guacamole at https://<url or IP>/guac and change the Default User"
 echo "Default user credentials: guacadmin/guacadmin"
 echo "You can use your own certificates by placing the private key in /opt/guac_services/nginx/ssl/self-ssl.key and the cert in /opt/guac_services/nginx/ssl/self.cert"
 echo "You can customize your Webpage by changing the index.html in /opt/guac_services/nginx/data/www/ and by editing /opt/guac_services/nginx/mysite.template"
