bbe19e98af329636eb40d34dfff4ce570d703e42
Ubuntu <ubuntu@ip-172-31-31-120.us-east-2.compute.internal>
2021-09-17 13:11:46 +0000

    updated stuff

--- reset.sh
+++ reset.sh
@@ -6,8 +6,9 @@ echo "          delete your certs files       (./nginx/ssl/)"
 echo ""
 read -p "Are you sure? " -n 1 -r
 echo ""   # (optional) move to a new line
+WD="/opt/guac_services"
 if [[ $REPLY =~ ^[Yy]$ ]]; then # do dangerous stuff
- chmod -R +x -- ./init
- sudo rm -r -f ./data/ ./drive/ ./record/ ./nginx/ssl/
+ chmod -R +x -- $WD/init
+ sudo rm -r -f $WD/data/ $WD/drive/ $WD/record/ $WD/nginx/ssl/
 fi
 
